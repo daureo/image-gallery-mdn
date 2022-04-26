93cf3549b6749bfc503c81e6bd07fc63ab80a58c
Daureo Silva <daureocosta@gmail.com>
2022-04-26 08:00:48 -0300

    Fixing first thumb not showing

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const imgList = ['../images/pic1.jpg',
+const imgList = ['images/pic1.jpg',
                 'images/pic2.jpg',
                 'images/pic3.jpg',
                 'images/pic4.jpg',
